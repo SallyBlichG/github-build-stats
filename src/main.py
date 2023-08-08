708b368658cec0af692b708489895f459b9b114b
SallyBlichG <sally.b@guardiosecurity.com>
2023-08-08 14:09:07 +0300

    add bigquery support for pushing the github stats, revert request to http

--- src/main.py
+++ src/main.py
@@ -1,23 +1,32 @@
+import http.client
 import json
-from datetime import datetime
 import os
-import requests
+from datetime import datetime
+from google.cloud import bigquery
 
 
 def main():
+    getBuildStatsGithub()
+
+
+def getBuildStatsGithub():
     try:
-        url = f"https://api.github.com/repos/{os.getenv('GITHUB_ORG_ID')}/{os.getenv('GITHUB_REPOSITORY_NAME')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/jobs"
-        payload = {}
+        conn = http.client.HTTPSConnection("api.github.com")
+        # http.client.HTTPSConnection.debuglevel = 1
+        payload = ''
         headers = {
             'Accept': 'application/vnd.github.v3+json',
             'Authorization': f"Bearer {os.getenv('GITHUB_TOKEN')}",
             'User-Agent': 'github-build-stats/1.0'
         }
-        response = requests.request("GET", url, headers=headers, data=payload)
-        res = response.text
+        conn.request("GET",
+                     f"/repos/{os.getenv('GITHUB_ORG_ID')}/{os.getenv('GITHUB_REPOSITORY_NAME')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/jobs",
+                     payload, headers)
+        res = conn.getresponse()
         data = res.read()
         obj = json.loads(data.decode("utf-8"))
         datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+        json_object = []
         for row in obj['jobs']:
             for step in range(len(row['steps'])):
                 if row['steps'][step]['status'] == "completed":
@@ -27,6 +36,23 @@ def main():
                     if time_difference.total_seconds() != 0.0:
                         print(row['steps'][step]["name"])
                         print(f"This step took: {time_difference.total_seconds()} seconds")
+                        data = {'timestamp': datetime.strptime(row['steps'][step]['started_at'], datetime_format), 'name': row['steps'][step]["name"], 'total_time': time_difference.total_seconds()}
+                        json_object.append(data)
+        writeStatsToBQ(json_object)
+    except Exception as e:
+        print("An exception occurred:", e)
+        exit(1)
+
+
+def writeStatsToBQ(json_object):
+    try:
+        client = bigquery.Client()
+        table_id = f"{os.getenv('GOOGLE_PROJECT_NAME')}.{os.getenv('BQ_DATASET')}.{os.getenv('BQ_TABLE')}"
+        errors = client.insert_rows_json(table_id, json_object)
+        if not errors:
+            print("New rows have been added for Datadog Source.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
 
     except Exception as e:
         print("An exception occurred:", e)
