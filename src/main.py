515d28cf7547f4eb092f2d9c15a3ca8f61caf351
SallyBlichG <sally.b@guardiosecurity.com>
2023-08-08 12:02:30 +0300

    change http client to requests

--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
-import http.client
 import json
 from datetime import datetime
 import os
+import requests
 
 
 def main():
     try:
-        conn = http.client.HTTPSConnection("api.github.com")
-        # http.client.HTTPSConnection.debuglevel = 1
-        payload = ''
+        url = f"/repos/{os.getenv('GITHUB_ORG_ID')}/{os.getenv('GITHUB_REPOSITORY_NAME')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/jobs"
+        payload = {}
         headers = {
             'Accept': 'application/vnd.github.v3+json',
             'Authorization': f"Bearer {os.getenv('GITHUB_TOKEN')}",
             'User-Agent': 'github-build-stats/1.0'
         }
-        conn.request("GET",
-                     f"/repos/{os.getenv('GITHUB_ORG_ID')}/{os.getenv('GITHUB_REPOSITORY_NAME')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/jobs",
-                     payload, headers)
-        res = conn.getresponse()
+        response = requests.request("GET", url, headers=headers, data=payload)
+        res = response.text
         data = res.read()
         obj = json.loads(data.decode("utf-8"))
         datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
@@ -29,7 +26,7 @@ def main():
                     time_difference = datetime_obj2 - datetime_obj1
                     if time_difference.total_seconds() != 0.0:
                         print(row['steps'][step]["name"])
-                        print(f"This step took: {time_difference.total_seconds()} seconds.")
+                        print(f"This step took: {time_difference.total_seconds()} seconds")
 
     except Exception as e:
         print("An exception occurred:", e)
