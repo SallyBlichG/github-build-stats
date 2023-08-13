b28115b1dac6ab9c5df5803164ee1e94a00c36d8
SallyBlichG <sally.b@guardiosecurity.com>
2023-08-13 10:14:23 +0300

    fix naming of vars and functions

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ from google.cloud import bigquery
 
 
 def main():
-    getBuildStatsGithub()
+    get_build_state_github()
 
 
-def getBuildStatsGithub():
+def get_build_state_github():
     try:
         conn = http.client.HTTPSConnection("api.github.com")
         payload = ''
@@ -25,34 +25,34 @@ def getBuildStatsGithub():
         data = res.read()
         obj = json.loads(data.decode("utf-8"))
         datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
-        json_object = []
+        json_object_bq_data = []
         for row in obj['jobs']:
             workflow_name = row['workflow_name']
             job_name = (row['name']).replace("\\", "")
             url = row['html_url']
             for step in range(len(row['steps'])):
                 if row['steps'][step]['status'] == "completed":
-                    datetime_obj1 = datetime.strptime(row['steps'][step]['started_at'], datetime_format)
-                    datetime_obj2 = datetime.strptime(row['steps'][step]['completed_at'], datetime_format)
-                    time_difference = datetime_obj2 - datetime_obj1
+                    datetime_started_at = datetime.strptime(row['steps'][step]['started_at'], datetime_format)
+                    datetime_completed_at = datetime.strptime(row['steps'][step]['completed_at'], datetime_format)
+                    time_difference = datetime_completed_at - datetime_started_at
                     if time_difference.total_seconds() != 0.0:
                         print(row['steps'][step]["name"])
                         print(f"This step took: {time_difference.total_seconds()} seconds")
-                        data = {'timestamp': (datetime_obj1.isoformat()),
+                        bq_data = {'timestamp': (datetime_started_at.isoformat()),
                                 'workflow_name': workflow_name, 'job_name': job_name,
                                 'step_name': row['steps'][step]["name"], 'total_time': time_difference.total_seconds(), 'url': url}
-                        json_object.append(data)
-        writeStatsToBQ(json_object)
+                        json_object_bq_data.append(bq_data)
+        write_stats_to_bq(json_object_bq_data)
     except Exception as e:
         print("An exception occurred:", e)
         exit(1)
 
 
-def writeStatsToBQ(json_object):
+def write_stats_to_bq(json_object_bq_data):
     try:
         client = bigquery.Client.from_service_account_json(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-        table_id = f"{os.getenv('GOOGLE_PROJECT_NAME')}.{os.getenv('BQ_DATASET')}.{os.getenv('BQ_TABLE')}"
-        errors = client.insert_rows_json(table_id, json_object)
+        bq_details = f"{os.getenv('GOOGLE_PROJECT_NAME')}.{os.getenv('BQ_DATASET')}.{os.getenv('BQ_TABLE')}"
+        errors = client.insert_rows_json(bq_details, json_object_bq_data)
         if not errors:
             print("New rows have been added")
         else:
